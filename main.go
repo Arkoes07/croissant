3860091ec8adef9b3f1e02491ffa75c8cb341dd4
M. Alwi Sukra <alwiesukra@gmail.com>
2021-07-25 22:08:02 +0700

    init and call from main

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 
 	"github.com/Arkoes07/croissant/pkg/secret"
 	"github.com/Arkoes07/croissant/pkg/secret/jsonsecret"
+	"github.com/Arkoes07/croissant/pkg/song"
+	"github.com/Arkoes07/croissant/pkg/song/spotifyservice"
 )
 
 func main() {
@@ -35,6 +37,26 @@ func main() {
 		log.Fatalf("[main] Fail to parse secret, err := %v\n", err)
 	}
 
-	// TODO: remove this line if secret has been used
-	log.Printf("secret: %+v\n", secret)
+	// initialize song service
+	var songSvc song.Service
+	{
+		// TODO: get config value from config file
+		cfg := spotifyservice.Config{
+			PlaylistID: "37i9dQZF1DXcBWIGoYBM5M",
+			SongsCount: 10,
+		}
+		songSvc, err = spotifyservice.New(secret.Spotify.ClientID, secret.Spotify.ClientSecret, cfg)
+		if err != nil {
+			log.Fatalf("[main] Fail to init song service, err := %v\n", err)
+		}
+	}
+
+	// TODO: will remove soon, only for demo purpose
+	songs, err := songSvc.GetSongs()
+	if err != nil {
+		log.Fatalf("[main] Fail to get songs, err := %v\n", err)
+	}
+	for _, song := range songs {
+		log.Printf("%+v\n", song)
+	}
 }
